d814e96ac04aab4feaee5f88d21c8bc3b97ac507
Wang Zhexue <2140967451@qq.com>
2025-06-17 10:41:15 +0800

    20250617

--- test1.cpp
+++ test1.cpp
@@ -1,123 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-char posChar(int idx) { return idx == 0 ? 'L' : (idx == 1 ? 'M' : 'R'); }
-
-struct State {
-  array<string, 3> comb;
-  string key() const { return comb[0] + "|" + comb[1] + "|" + comb[2]; }
-};
-
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  int T;
-  cin >> T;
-  while (T--) {
-    int fig[3];
-    for (int i = 0; i < 3; i++) {
-      cin >> fig[i];
+  mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+  uniform_int_distribution<int> distA(1, 1000000000);
+
+  int T = 10;
+  int rem = 200000;
+  cout << T << "\n";
+  for (int tc = 0; tc < T; tc++) {
+    int max_n = min(rem - (T - tc - 1), 100000);
+    int n = uniform_int_distribution<int>(1, max_n)(rng);
+    rem -= n;
+
+    int max_k = (n + 1) / 2;
+    int k = uniform_int_distribution<int>(0, max_k)(rng);
+    if (k == n) k = n - 1;
+
+    cout << n << " " << k << "\n";
+    for (int i = 0; i < n; i++) {
+      cout << distA(rng) << (i + 1 == n ? "\n" : " ");
     }
-    auto makeTarget = [&](int forbidden) -> string {
-      vector<char> v;
-      for (char d : {'0', '3', '4'}) {
-        if (d - '0' != forbidden) v.push_back(d);
-      }
-      sort(v.begin(), v.end());
-      return string(v.begin(), v.end());
-    };
-    array<string, 3> target;
-    target[0] = makeTarget(fig[0]);
-    target[1] = makeTarget(fig[1]);
-    target[2] = makeTarget(fig[2]);
-    State init;
-    for (int i = 0; i < 3; i++) {
-      string s;
-      cin >> s;
-      sort(s.begin(), s.end());
-      init.comb[i] = s;
-    }
-    using Path = vector<string>;
-    queue<pair<State, Path>> qu;
-    unordered_set<string> vis;
-    qu.push({init, {}});
-    vis.insert(init.key());
-
-    bool found = false;
-    Path ans;
-
-    while (!qu.empty() && !found) {
-      auto cur = qu.front();
-      qu.pop();
-      State s = cur.first;
-      Path path = cur.second;
-      if (!path.empty() && s.comb[0] == target[0] && s.comb[1] == target[1] &&
-          s.comb[2] == target[2]) {
-        ans = path;
-        found = true;
-        break;
-      }
-      vector<tuple<string, State, Path>> nextStates;
-      for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-          if (i == j) continue;
-          for (char x : s.comb[i]) {
-            for (char y : s.comb[j]) {
-              if (x == y) continue;
-              State ns = s;
-              {
-                string temp = ns.comb[i];
-                for (auto &ch : temp) {
-                  if (ch == x) {
-                    ch = y;
-                    break;
-                  }
-                }
-                sort(temp.begin(), temp.end());
-                ns.comb[i] = temp;
-              }
-              {
-                string temp = ns.comb[j];
-                for (auto &ch : temp) {
-                  if (ch == y) {
-                    ch = x;
-                    break;
-                  }
-                }
-                sort(temp.begin(), temp.end());
-                ns.comb[j] = temp;
-              }
-              string op;
-              op.push_back(x);
-              op += "->";
-              op.push_back(posChar(i));
-              op += " ";
-              op.push_back(y);
-              op += "->";
-              op.push_back(posChar(j));
-
-              Path np = path;
-              np.push_back(op);
-
-              nextStates.push_back({op, ns, np});
-            }
-          }
-        }
-      }
-      sort(nextStates.begin(), nextStates.end(),
-           [](auto &a, auto &b) { return get<0>(a) < get<0>(b); });
-
-      for (auto &entry : nextStates) {
-        State ns = get<1>(entry);
-        string key = ns.key();
-        if (vis.count(key)) continue;
-        vis.insert(key);
-        qu.push({ns, get<2>(entry)});
-      }
-    }
-    for (auto &s : ans) cout << s << "\n";
-    cout << "---------" << endl;
   }
   return 0;
 }
---
+++ 紫.cpp
@@ -0,0 +1,206 @@
+#include <bits/extc++.h>
+#include <bits/stdc++.h>
+using namespace std;
+using namespace __gnu_pbds;
+#define endl '\n'
+#define ture true
+#define flase false
+#define pow power
+#define all(x) begin(x), end(x)
+#define mem(a, x) memset(a, x, sizeof(a))
+#define sz(x) (int)x.size()
+#define lowbit(x) (x & -x)
+#define time(a, b) (abs((b - a) / CLOCKS_PER_SEC))
+// double a = clock();
+#define pb push_back
+#define EPS 1e-7
+#define int ll
+#define ll long long
+#define i64 long long
+#define i128 __int128
+#define fr first
+#define sc second
+#define tcT template <class T
+#define tcTU tcT, class U
+#define tcTV tcT, class... Ts
+
+void unsyncIO() { cin.tie(0)->sync_with_stdio(0); }
+void setPrec() { cout << fixed << setprecision(15); }
+void setIO() { unsyncIO(), setPrec(); }
+
+tcT > T gcd(const T &a, const T &b) { return b ? gcd(b, a % b) : a; }
+tcTU > T div(T a, T b, U flag) {
+  if (flag) {
+    return a / b + ((a ^ b) > 0 && a % b);
+  } else {
+    return a / b - ((a ^ b) < 0 && a % b);
+  }
+}
+
+tcT > using V = vector<T>;
+tcTU > using PR = pair<T, U>;
+tcTU > using MP = map<T, U>;
+tcTU > using VP = vector<pair<T, U>>;
+tcT > using pql =
+    __gnu_pbds::priority_queue<T, less<T>, __gnu_pbds::pairing_heap_tag>;
+tcT > using pqg =
+    __gnu_pbds::priority_queue<T, greater<T>, __gnu_pbds::pairing_heap_tag>;
+
+tcTU > istream &operator>>(istream &in, pair<T, U> &a) {
+  return in >> a.first >> a.second;
+}
+
+tcT > istream &operator>>(istream &in, vector<T> &a) {
+  for (auto &x : a) {
+    in >> x;
+  }
+  return in;
+}
+
+tcTU > ostream &operator<<(ostream &out, const pair<T, U> &a) {
+  return out << a.first << ' ' << a.second;
+}
+
+tcTU > ostream &operator<<(ostream &out, const vector<pair<T, U>> &a) {
+  for (auto &x : a) {
+    out << x << endl;
+  }
+  return out;
+}
+
+tcT > ostream &operator<<(ostream &out, const vector<T> &a) {
+  int n = a.size();
+  if (!n) {
+    return out;
+  }
+  out << a[0];
+  for (int i = 1; i < n; i++) {
+    out << ' ' << a[i];
+  }
+  return out;
+}
+
+std::ostream &operator<<(std::ostream &os, i128 n) {
+  std::string s;
+  while (n) {
+    s += '0' + n % 10;
+    n /= 10;
+  }
+  std::reverse(s.begin(), s.end());
+  return os << s;
+}
+
+inline int power(int a, int b, int p = 1e9 + 7) {
+  int res = 1;
+  for (; b; b /= 2, a = 1LL * a * a % p) {
+    if (b % 2) {
+      res = 1LL * res * a % p;
+    }
+  }
+  return res;
+}
+
+tcTV > bool ckmin(T &a, const T &b, const Ts &...args) {
+  bool changed = false;
+  if (b < a) {
+    a = b;
+    changed = true;
+  }
+  (void)std::initializer_list<int>{
+      ((args < a ? (a = args, changed = true) : 0), 0)...};
+  return changed;
+}
+
+tcTV > bool ckmax(T &a, const T &b, const Ts &...args) {
+  bool changed = false;
+  if (a < b) {
+    a = b;
+    changed = true;
+  }
+  (void)std::initializer_list<int>{
+      ((a < args ? (a = args, changed = true) : 0), 0)...};
+  return changed;
+}
+
+tcT > void remDup(vector<T> &v) {
+  sort(all(v));
+  v.erase(unique(all(v)), end(v));
+}
+
+tcTU > void erase(T &t, const U &u) {
+  auto it = t.find(u);
+  assert(it != end(t));
+  t.erase(it);
+}
+
+tcTU > T fstTrue(T lo, T hi, U f) {
+  hi++;
+  assert(lo <= hi);
+  while (lo < hi) {
+    T mid = lo + (hi - lo) / 2;
+    f(mid) ? hi = mid : lo = mid + 1;
+  }
+  return lo;
+}
+
+tcTU > T lstTrue(T lo, T hi, U f) {
+  lo--;
+  assert(lo <= hi);
+  while (lo < hi) {
+    T mid = lo + (hi - lo + 1) / 2;
+    f(mid) ? lo = mid : hi = mid - 1;
+  }
+  return lo;
+}
+
+constexpr int modulo[] = {998244353, 1000000007};
+constexpr int mod = modulo[1];
+constexpr int inf = 0x7fffffff;
+constexpr int N = 1.01e6;
+constexpr int M = 2.01e3;
+
+#ifdef LOCAL
+#include <C:/Users/70510/Desktop/Others/algo/debug.h>
+#else
+#define debug(...) 42
+#endif
+
+void solve() {
+  long long n;
+  string s;
+  cin >> n >> s;
+  int A = 1, C = 1, B = 0;
+  int dp0 = 1, dp1 = 0;
+  for (auto c : s) {
+    if (c == '0') {
+      A = A * 2 % mod;
+      dp0 = dp0 * 2 % mod;
+    } else {
+      C = C * 2 % mod;
+      dp1 = (dp1 * 2 + dp0) % mod;
+    }
+  }
+  B = dp1;
+  int An = pow(A, n);
+  int Cn = pow(C, n);
+  int dp1n;
+  if ((A - C) % mod != 0) {
+    int denom = (A - C + mod) % mod;
+    int invd = pow(denom, mod - 2);
+    dp1n = B * ((An - Cn + mod) % mod) % mod * invd % mod;
+  } else {
+    dp1n = B * (n % mod) % mod * pow(A, (n - 1) % (mod - 1)) % mod;
+  }
+  int ans = (An + dp1n - 1 + mod) % mod;
+  cout << ans << endl;
+}
+
+signed main() {
+  setIO();
+  int tt = 1;
+  cin >> tt;
+  while (tt--) {
+    solve();
+  }
+  return 0;
+}
